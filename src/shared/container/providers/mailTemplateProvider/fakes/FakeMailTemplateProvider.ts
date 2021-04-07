3a5af3811342246a003c39ca3b1518104509c259
Antonio Junior <antonio.jr.ssouza@gmail.com>
2021-04-06 20:27:00 -0400

    Adding email template to forgot password operation

--- src/shared/container/providers/mailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/mailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,11 +1,7 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '../interfaces/IMailTemplateProvider';
 
 export default class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({
-    template,
-    variables,
-  }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail Content';
   }
 }
