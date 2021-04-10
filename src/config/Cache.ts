134ebd3bb0e8bfa6020d64443ea51552b3aa0617
Antonio Junior <antonio.jr.ssouza@gmail.com>
2021-04-10 18:39:57 -0400

    Adding feature to rate limit of user requests

--- src/config/Cache.ts
+++ src/config/Cache.ts
@@ -11,9 +11,9 @@ export default {
   driver: process.env.CACHE_DRIVER as string | 'redis',
   config: {
     redis: {
-      host: process.env.CACHE_HOST,
-      port: process.env.CACHE_PORT,
-      password: process.env.CACHE_PASSWORD,
+      host: process.env.REDIS_HOST as string,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
     },
   },
 } as ICacheConfig;
